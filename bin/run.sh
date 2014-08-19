dfaa9cfafd43a5aad2959366cd14b818868ccb72
Marc Villacorta <marc.villacorta@gmail.com>
2014-08-19 11:09:58 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SUDO=`which sudo`; if [[ $EUID -ne 0 ]]; then $SUDO $0 $@; else
-  CID=$(docker run --privileged -d h0tbird/dnsmasq)
+  CID=$(docker run --privileged -d h0tbird/boot)
   pipework br0 $CID 192.168.2.10/24
 fi
