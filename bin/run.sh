d0255e7e75e939283c06aabd476791167e8dce64
Marc Villacorta <marc.villacorta@gmail.com>
2014-08-20 18:26:13 +0200

    Set container hostname

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,6 @@
 
 SUDO=`which sudo`; if [[ $EUID -ne 0 ]]; then $SUDO $0 $@; else
   NAME='boot'
-  docker run --privileged -d --name $NAME h0tbird/boot
+  CID=$(docker run --privileged -d -h $NAME --name $NAME h0tbird/boot)
   pipework br0 $NAME 192.168.2.10/24
 fi
