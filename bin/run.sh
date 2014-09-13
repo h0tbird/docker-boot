90d7bb83bdae99e035b025878cdec582264adea3
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-13 11:11:32 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -7,9 +7,11 @@ SUDO=`which sudo`; if [[ $EUID -ne 0 ]]; then $SUDO $0 $@; else
 
   LIST=`docker ps -a | awk '/'$ROLE'[0-9]/ {print $NF}'`
   for ID in `seq -w 99`; do echo $LIST | grep -q $ID || break; done
-  NAME=${ROLE}${ID}.${DOMAIN}
 
-  CID=$(docker run --privileged -d -h $NAME --name $NAME h0tbird/boot)
-  pipework br0 $NAME 192.168.2.10/24
+  HNAME=${ROLE}${ID}.${DOMAIN}
+  CNAME=${ROLE}${ID}
+
+  CID=$(docker run --privileged -d -h $HNAME --name $CNAME h0tbird/boot)
+  pipework br0 $CNAME 192.168.2.10/24
 
 fi
